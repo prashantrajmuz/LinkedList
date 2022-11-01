392d765b952d3e77503a5819edda41026d81665d
prashantrajmuz <36060626+prashantrajmuz@users.noreply.github.com>
2022-11-01 14:41:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Seive-of-eratosthenes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-6fx6jv)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let a=10;
+let b=10;
+let c = a+b;
+console.log(c);
